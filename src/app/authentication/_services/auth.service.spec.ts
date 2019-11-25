231ee9fcbc21652348bc209e5d6f68be9b5a932c
Steve VandenBrink <steve@vandenbrinksoftware.com>
2019-11-25 10:40:44 -0500

    Added service test for login

--- src/app/authentication/_services/auth.service.spec.ts
+++ src/app/authentication/_services/auth.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AuthService } from './auth.service';
+import { of } from 'rxjs';
 
 describe('AuthService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -12,4 +13,22 @@ describe('AuthService', () => {
     const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
   });
+
+  it('should return token on login', () => {
+    const service: AuthService = TestBed.get(AuthService);
+    const loginResponse = {
+      result: {
+        tokenString: ''
+      }
+    };
+    let response;
+    spyOn(service, 'login').and.returnValue(of(loginResponse));
+
+    service.login({username: 'some-user', password: 'password'}).subscribe(res => {
+      response = res;
+    });
+
+    expect(response).toEqual(loginResponse);
+  });
+
 });
